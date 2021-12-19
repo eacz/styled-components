c96fd1015ed097ae21e5854f58f446384cafc4fa
Esteban Canteros <estebanacanteros@gmail.com>
2021-12-19 17:48:48 -0300

    global styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,25 @@
 import { Button } from "components/common";
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  p {
+    font-size: 8px;
+  }
+
+  body {
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script', cursive;
+  }
+
+`
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>App</h1>
       <Button>
         Test
