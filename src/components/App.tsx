0c3178302e4cb7163330821a434a53ced93f7c26
Esteban Canteros <estebanacanteros@gmail.com>
2021-12-19 17:40:29 -0300

    first styled component

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "components/common";
+
+function App() {
+  return (
+    <>
+      <h1>App</h1>
+      <Button>
+        Test
+      </Button>
+    </>
+  );
+}
+
+export default App;
